1d81ca7cb5cab7989e5fc28f91a2e03356c89b5d
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-11-28 18:50:49 +0200

    create scanner component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "./App.css";
-import Camera from "./components/Camera";
+import Scanner from "./components/Scanner";
 
 function App() {
-	return <div className="App">{<Camera />}</div>;
+	const [camera, setCamera] = useState(false);
+	const [result, setResult] = useState(null);
+
+	const onDetected = (result) => {
+		setResult(result);
+	};
+	return (
+		<div className="App">
+			<p>{result ? result : "Scanning..."}</p>
+			<button onClick={() => setCamera(!camera)}>
+				{camera ? "Stop" : "Start"}
+			</button>
+			<div className="container">
+				{camera && <Scanner onDetected={onDetected} />}
+			</div>
+		</div>
+	);
 }
 
 export default App;
